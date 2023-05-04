4e46f2697db3a33e2e0156d5cb9627199c916a5c
Vlad Tarnovski <vladtarnovski989@gmail.com>
2023-05-04 20:44:24 +0300

    feat: add base colors

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,11 +7,7 @@ function Header(): JSX.Element {
   return (
     <div className="header flex justify-between items-center shadow-lg bg-gray-400/20 pl-[2%] pr-[2%] mb-6">
       <div className="header__logo overflow-hidden">
-        <img
-          src={LogoImg}
-          alt="Rick and Morty"
-          className="block w-60 h-20 mt-[-10px]"
-        />
+        <img src={LogoImg} alt="Rick and Morty" className="block w-60 h-20 mt-[-10px]" />
         <div className="text-sm font-thin text-teal-500 shadow-yellow-300/60 shadow-lg mt-[-3px]">
           GraphQL IDE
         </div>
