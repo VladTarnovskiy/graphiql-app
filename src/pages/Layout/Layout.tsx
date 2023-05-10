184461d6406769f8d04a15abe2be58fd3a75be88
Vlad Tarnovski <vladtarnovski989@gmail.com>
2023-05-10 17:28:27 +0300

    feat: add loader

--- src/pages/Layout/Layout.tsx
+++ src/pages/Layout/Layout.tsx
@@ -2,13 +2,14 @@ import { Outlet } from 'react-router-dom';
 import Header from 'src/components/Header/Header';
 import Footer from 'src/components/Footer/Footer';
 import { Suspense } from 'react';
+import Loader from 'src/components/Loader/Loader';
 
 function Layout(): JSX.Element {
   return (
     <>
       <Header />
       <div className="wrapper pl-[2%] pr-[2%] w-full">
-        <Suspense fallback={<div>Loading...</div>}>
+        <Suspense fallback={<Loader />}>
           <Outlet />
         </Suspense>
       </div>
