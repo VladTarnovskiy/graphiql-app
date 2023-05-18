0ffb3d16e374b786d6b2e379d1a7c7696a8e2129
AlexGorSer <taskekaminari@gmail.com>
2023-05-18 16:04:31 +0200

    feat: add color for active link

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,18 +1,18 @@
 import { NavLink } from 'react-router-dom';
 
 function Navigation(): JSX.Element {
+  const classStyle =
+    'navigation__item hover:text-base_green_light mr-[20px] ml-[5px] text-[#374151] dark:text-base_white';
+
+  const handleActiveLink = (isActive: boolean) => {
+    return isActive ? `${classStyle} text-base_green_light` : classStyle;
+  };
   return (
     <div className="navigation font-thin dark:text-base_white">
-      <NavLink
-        className="navigation__item hover:text-base_green_light mr-[20px] ml-[5px] text-[#374151] dark:text-base_white"
-        to="/"
-      >
+      <NavLink className={({ isActive }) => handleActiveLink(isActive)} to="/">
         <span className="hover:text-base_green_light">Welcome</span>
       </NavLink>
-      <NavLink
-        className="navigation__item hover:text-base_green_light mr-[20px] ml-[5px] text-[#374151] dark:text-base_white"
-        to="/graphi-ql"
-      >
+      <NavLink className={({ isActive }) => handleActiveLink(isActive)} to="/graphi-ql">
         <span className="hover:text-base_green_light">GraphiQL</span>
       </NavLink>
     </div>
