dab342d4d4ae03b341b0794d891d09173e692eaa
Vlad Tarnovski <vladtarnovski989@gmail.com>
2023-05-21 18:01:12 +0300

    feat: add Error boundary component

---
+++ src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,40 @@
+import React, { ErrorInfo, ReactNode } from 'react';
+
+interface Props {
+  children?: ReactNode;
+  fallBackUIComponent: ReactNode;
+}
+
+interface State {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      hasError: false,
+    };
+  }
+
+  static getDerivedStateFromError(error: Error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    console.log('Catched error', errorInfo);
+  }
+
+  render() {
+    const { hasError } = this.state;
+    const { children, fallBackUIComponent } = this.props;
+
+    if (hasError) {
+      return fallBackUIComponent;
+    }
+
+    return children;
+  }
+}
+
+export default ErrorBoundary;
--- src/pages/Layout/Layout.tsx
+++ src/pages/Layout/Layout.tsx
@@ -3,23 +3,27 @@ import Header from 'src/components/Header/Header';
 import Footer from 'src/components/Footer/Footer';
 import { Suspense } from 'react';
 import Loader from 'src/components/Loader/Loader';
+import ErrorBoundary from 'src/components/ErrorBoundary/ErrorBoundary';
+import FallBackUIComponent from 'src/components/ErrorBoundary/FallBackUIComponent/FallBackUIComponent';
 
 function Layout(): JSX.Element {
   return (
     <>
       <Header />
-      <main className="wrapper pl-[2%] pr-[2%] w-full mb-[13px] dark:bg-base_dark">
-        <Suspense
-          fallback={
-            <div className="m-auto w-fit mt-[30vh]">
-              <Loader />
-            </div>
-          }
-        >
-          <Outlet />
-        </Suspense>
-      </main>
-      <Footer />
+      <ErrorBoundary fallBackUIComponent={<FallBackUIComponent />}>
+        <main className="wrapper pl-[2%] pr-[2%] w-full mb-[13px] dark:bg-base_dark">
+          <Suspense
+            fallback={
+              <div className="m-auto w-fit mt-[30vh]">
+                <Loader />
+              </div>
+            }
+          >
+            <Outlet />
+          </Suspense>
+        </main>
+        <Footer />
+      </ErrorBoundary>
     </>
   );
 }
