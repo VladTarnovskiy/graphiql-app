47d034cb724e09880e128aa615b9739067f29be6
Dzianis Karnachenka <arcadievich13051992@gmail.com>
2023-05-23 09:38:04 +0300

    fix: correct dark theme

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,10 +1,11 @@
+import { t } from 'i18next';
 import style from './notFound.module.scss';
 
 function NotFound(): JSX.Element {
   return (
-    <div className={style.error_wrapper}>
+    <div className={`${style.error_wrapper} dark:text-base_white`}>
       <div className={style.error_code}>404</div>
-      <div className={style.error_description}>The page you are looking for not found!</div>
+      <div className={style.error_description}>{t(`404`)}</div>
       <div className={style.error_animation} />
     </div>
   );
