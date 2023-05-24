f8d9f2f0b138d3c3585f693a7392c25c902fbee4
Vlad Tarnovski <vladtarnovski989@gmail.com>
2023-05-24 23:16:31 +0300

    fix: layout

--- src/components/WelcomeCards/DeveloperCard.test.tsx
+++ src/components/WelcomeCards/DeveloperCard.test.tsx
@@ -1,5 +1,5 @@
-import { describe, it, expect, vi } from 'vitest';
-import { render, screen, waitFor } from '@testing-library/react';
+import { describe, it, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
 
 import { Provider } from 'react-redux';
 import { store } from 'src/app/store';
