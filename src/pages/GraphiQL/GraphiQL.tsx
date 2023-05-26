59e961bd64530e20727a36c7f5d9aed2e446d6a0
vladtarnovskiy <93903876+VladTarnovskiy@users.noreply.github.com>
2023-05-26 13:58:40 +0300

    fix: documents background color bug

--- src/pages/GraphiQL/GraphiQL.tsx
+++ src/pages/GraphiQL/GraphiQL.tsx
@@ -96,7 +96,7 @@ function GraphiQLPage(): JSX.Element {
           <SettingModal />
         </Modal>
       )}
-      <div className="absolute left-0 top-0 instruments z-10 flex justify-start p-2 pl-[8px] md:pl-[7px] docs-nav h-full dark:bg-dark_textarea w-[60px] md:w-[50px] min-h-[79vh] shadow-lg shadow-base_green/50 rounded-r-md bg-base_white">
+      <div className="absolute left-0 top-0 instruments z-10 flex justify-start p-2 pl-[8px] md:pl-[7px] docs-nav h-full dark:bg-dark_textarea min-w-[60px] md:min-w-[50px] min-h-[79vh] shadow-lg shadow-base_green/50 rounded-r-md bg-base_white">
         <div className="flex flex-col justify-start">
           <Tooltip
             id="button-tooltip"
